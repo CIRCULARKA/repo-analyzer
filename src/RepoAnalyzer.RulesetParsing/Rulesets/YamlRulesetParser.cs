a46f2b3939cf7809b622eb4ac55068d5749e0c12
CIRCULARKA <donkardon@list.ru>
2026-01-10 18:29:59 +0500

    chore: Add library for YAML parsing

---
+++ src/RepoAnalyzer.RulesetParsing/Rulesets/YamlRulesetParser.cs
@@ -0,0 +1,12 @@
+namespace RepoAnalyzer.RulesetParsing.Rulesets;
+
+/// <summary>
+/// Ruleset parser that parses ruleset from YAML format
+/// </summary>
+public class YamlRulesetParser
+{
+    public Ruleset ParseRuleset()
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/RepoAnalyzer.RulesetParsing/Usings.cs
+++ src/RepoAnalyzer.RulesetParsing/Usings.cs
@@ -1,2 +1,4 @@
 global using RepoAnalyzer.Core.Rules;
 global using RepoAnalyzer.Core.Rulesets;
+global using YamlDotNet;
+global using YamlDotNet.Serialization;
